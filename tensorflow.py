cd7d2e28119bf990fb790558807e8f0e04507e8a
Hiroshi9876 <161133047+Hiroshi9876@users.noreply.github.com>
2024-04-18 07:29:58 +0900

    Create tensorflow.py

---
+++ tensorflow.py
@@ -0,0 +1,15 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import matplotlib.pyplot as pyplot
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+print(len(train_labels))
